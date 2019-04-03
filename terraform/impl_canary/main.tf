342124f20bd95058041e729ff59da5146e8b1795
jzulim <john.zulim@adhocteam.us>
2019-04-02 20:39:47 -0700

    TF config files and Jenkins build job for Canary deploys.

---
+++ terraform/impl_canary/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/templates/user_data.tpl")}"
+
+  vars {
+    env    = "${lower(var.env)}"
+    bucket = "${var.app_config_bucket}"
+  }
+}
+
+
+
+resource "aws_instance" "impl_canary_app" {
+
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+
+  subnet_id     = "${var.subnet_id}"
+  #user_data = "${file("${path.module}/templates/user_data")}"
+
+
+  user_data                   = "${data.template_file.user_data.rendered}"
+  iam_instance_profile        = "${var.iam_instance_profile}"
+
+  #   For canary, just needing one for ssh access: vpn
+
+  vpc_security_group_ids = [
+    "${var.vpc_sg_id}"
+  ]
+
+  associate_public_ip_address = false
+
+  tags {
+    Name = "bb-impl-canary"
+  }
+}
+
+#Assign Private IP to Output variable
+
+  output "private_ip" {
+    value = "${aws_instance.impl_canary_app.private_ip}"
+  }
---
+++ terraform/prod_canary/templates/user_data.tpl
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ set -e
+
+ exec 2> >(tee -a /var/log/boot.log >&2)
+
+ aws s3 cp s3://${bucket}/${env}/VAULT_PW .
+aws s3 cp s3://${bucket}/${env}/REPO_URI .
+
+ ansible-playbook --vault-password-file=./VAULT_PW \
+  -i "localhost" \
+  -e "env=${env}" \
+  -e "repo=$(cat REPO_URI)" \
+  /var/pyapps/hhs_o_server/env_config.yml
+
+ rm VAULT_PW REPO_URI
