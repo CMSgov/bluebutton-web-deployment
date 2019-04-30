56b7c7560ab55eba47ec8cca70bf854699913efd
John Zulim <john.zulim@adhocteam.us>
2019-04-30 09:45:00 -0700

    BLUEBUTTON-821: Slack Notifications (#1360)
    * Adding Slack notifications to cmsgov slack for BUILD and DEPLOY Jenkins jobs.

---
+++ Jenkinsfiles/helpers.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+// Send a message to Slack
+def slackNotify(message, status='unknown', channel='blue-button-api-alert') {
+  switch (status) {
+    case 'good':
+      slack_color = 'good'
+      break
+    case 'bad':
+      slack_color = 'danger'
+      break
+    default:
+      slack_color = null
+      break
+  }
+
+  withCredentials([string(credentialsId: 'bb20-slack-token', variable: 'slack_token')]) {
+    if (env.DISABLE_SLACK_ALERTS != 'true') {
+      slackSend message: "${message}\n${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)",
+        color: slack_color,
+        failOnError: false,
+        teamDomain: 'cmsgov',
+        channel: channel,
+        token: slack_token
+    }
+  }
+}
+
+return this
