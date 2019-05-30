ed8ad5e39ec1e2930f58c1e08637a8a730239fe4
John Zulim <john.zulim@adhocteam.us>
2019-05-30 14:56:21 -0700

    BLUEBUTTON-961: Prep TEST to match PROD (#1364)
    * Adding Canary Terraform Config and adding ENV to Canary pipeline.
    
    * Adding new relic key to TEST vault
    
    * Adding TEST environment to New Relic
    
    * Adding private VPN CIDR to allowed_ips in the vault for all env.

---
+++ terraform/test_canary/variables.tf
@@ -0,0 +1,17 @@
+variable "env" {}
+
+variable "app_config_bucket" {}
+
+variable "ami_id" {}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "subnet_id" {}
+
+variable "iam_instance_profile" {}
+
+variable "vpc_sg_id" {}
+
+variable "instance_name" {}
