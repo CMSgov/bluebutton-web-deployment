a305578d4d63a8886dc171b0e3741c1ae0d26c07
Nathan Dister <nathan.dister@adhocteam.us>
2021-01-12 13:34:35 -0500

    Rename files

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -2,7 +2,7 @@
 
 DOCKER_TAG=${1:-py36-an27-tf11}
 
-docker build --file Dockerfile.bb2-cbc-build \
+docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.6} \
   --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
