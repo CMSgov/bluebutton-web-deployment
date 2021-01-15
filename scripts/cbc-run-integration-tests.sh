16e64222d263a51d52f656d0ffc5f8a52796f3ad
Dave Tisza <dtisza@fearless.tech>
2021-01-15 09:06:35 -0500

    Test svr run in cbc-run-integration-tests.sh

--- scripts/cbc-run-integration-tests.sh
+++ scripts/cbc-run-integration-tests.sh
@@ -9,12 +9,51 @@ set -e
 branch="${BRANCH:-master}"
 
 echo
-echo Running $0
+echo Running script: $0
 echo
-echo Using BRANCH:  ${branch}
+echo Using ENV/environment variables:
+echo
+echo     BRANCH:  ${branch}
 echo 
-echo Using FHIR_URL:  ${FHIR_URL}
+echo     FHIR_URL:  ${FHIR_URL}
 echo 
-echo 
+echo
+
+# Clone the web server repo
+git clone https://github.com/CMSgov/bluebutton-web-server.git code
+
+# Checkout commit hash or branch
+pushd code
+git fetch origin "+refs/heads/master:refs/remotes/origin/master" "+refs/pull/*:refs/remotes/origin/pr/*"
+git checkout "$branch"
+
+# Install requirements
+virtualenv -ppython3 venv
+. venv/bin/activate
+pip install -r requirements/requirements.txt
+pip install sqlparse
+
+# Bootstrap the database, run the server
+export DJANGO_SETTINGS_MODULE="hhs_oauth_server.settings.dev"
+.././migrate.sh
+python3 manage.py runserver 0.0.0.0:8899 &
+RUNSERVER_PID="$!"
+popd
+
+echo
+echo Running webserver process PID: ${RUNSERVER_PID}
+echo
+
+sleep 60
+
+echo
+echo
+echo
+ls -lR
+echo
+echo
+echo
+
+
 echo SUCCESS
 exit 0
