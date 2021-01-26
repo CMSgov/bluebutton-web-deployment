f13a10353d6f521c5127a1db200afb033b0d00ee
Dave Tisza <dtisza@fearless.tech>
2021-01-26 16:58:54 -0500

    Create run script for cbc docker container
    - Create run script scripts/cbc-run-integration-tests.sh
      This calls a BB2 webserver scipt to run the tests.
    
    BB2-124

--- scripts/cbc-run-integration-tests.sh
+++ scripts/cbc-run-integration-tests.sh
@@ -4,75 +4,41 @@
 #
 # It runs the BB2 integration tests and returns a SUCCESS or FAIL result.
 
+# Echo function that includes script name on each line for console log readability
+echo_msg () {
+  echo "$(basename $0): $1"
+}
+
+
+# main
 set -e
 
 branch="${BRANCH:-master}"
 
-echo
-echo Running script: $0
-echo
-echo Using ENV/environment variables:
-echo
-echo     BRANCH:  ${branch}
-echo 
-echo     FHIR_URL:  ${FHIR_URL}
-echo 
-echo
+echo_msg
+echo_msg Running script: $0
+echo_msg
+echo_msg Using ENV/environment variables:
+echo_msg
+echo_msg     BRANCH:  ${branch}
+echo_msg 
+echo_msg     FHIR_URL:  ${FHIR_URL}
+echo_msg 
+echo_msg
 
-# Install Debian packages
-echo
-echo Install Debian packages:
-echo
-apt install python3 python3-venv
-
-# Cloning the web server repo under ./code directory
-echo
-echo Clone the web server repo under the ./code directory:
-echo
+# Cloning the web server repo under ./code directory.
+echo_msg
+echo_msg Clone the web server repo under the ./code directory:
+echo_msg
 git clone https://github.com/CMSgov/bluebutton-web-server.git code
 
-# Checkout commit hash or branch
-echo
-echo Checkout commit hash or branch from: branch = ${branch}
-echo
-pushd code
+# Checkout commit hash or branch.
+echo_msg
+echo_msg Checkout commit hash or branch from: branch = ${branch}
+echo_msg
+cd code
 git fetch origin "+refs/heads/master:refs/remotes/origin/master" "+refs/pull/*:refs/remotes/origin/pr/*"
 git checkout "$branch"
 
-# Setup Python virtual env and Install requirements:
-echo
-echo Setup Python virtual env and Install requirements:
-echo
-virtualenv -ppython3 venv
-. venv/bin/activate
-pip install -r requirements/requirements.txt
-pip install sqlparse
-
-# Run the Django web server:
-echo
-echo Run the Django web server:
-echo
-export DJANGO_SETTINGS_MODULE="hhs_oauth_server.settings.dev"
-.././migrate.sh
-python3 manage.py runserver 0.0.0.0:8899 &
-RUNSERVER_PID="$!"
-popd
-
-echo
-echo Running webserver process PID: ${RUNSERVER_PID}
-echo
-
-sleep 60
-
-echo
-echo
-echo
-ls -lR
-echo
-echo
-echo
-
-# Finish and perform any needed cleanup:
-
-echo SUCCESS
-exit 0
+# Call run tests script from webserver repo.
+sh docker-compose/run_integration_tests_inside_cbc_build_docker.sh
