726fc7003877688de99b4d6696b0c76fc273d674
Dave Tisza <dtisza@fearless.tech>
2021-01-27 09:11:26 -0500

    Fix echo_msg in cbc-run-integration-tests.sh

--- scripts/cbc-run-integration-tests.sh
+++ scripts/cbc-run-integration-tests.sh
@@ -6,7 +6,7 @@
 
 # Echo function that includes script name on each line for console log readability
 echo_msg () {
-  echo "$(basename $0): $1"
+  echo "$(basename $0): $*"
 }
 
 
@@ -16,25 +16,25 @@ set -e
 branch="${BRANCH:-master}"
 
 echo_msg
-echo_msg Running script: $0
+echo_msg "Running script: $0"
 echo_msg
-echo_msg Using ENV/environment variables:
+echo_msg "Using ENV/environment variables:"
 echo_msg
-echo_msg     BRANCH:  ${branch}
+echo_msg "    BRANCH:  ${branch}"
 echo_msg 
-echo_msg     FHIR_URL:  ${FHIR_URL}
+echo_msg "    FHIR_URL:  ${FHIR_URL}"
 echo_msg 
 echo_msg
 
 # Cloning the web server repo under ./code directory.
 echo_msg
-echo_msg Clone the web server repo under the ./code directory:
+echo_msg "Clone the web server repo under the ./code directory:"
 echo_msg
 git clone https://github.com/CMSgov/bluebutton-web-server.git code
 
 # Checkout commit hash or branch.
 echo_msg
-echo_msg Checkout commit hash or branch from: branch = ${branch}
+echo_msg "Checkout commit hash or branch from: branch = ${branch}"
 echo_msg
 cd code
 git fetch origin "+refs/heads/master:refs/remotes/origin/master" "+refs/pull/*:refs/remotes/origin/pr/*"
