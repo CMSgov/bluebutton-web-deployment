529fdcdcf3c5913669ced01f5080e5e8ab81d2b0
Don Seymour <don.seymour@adhocteam.us>
2021-04-30 12:10:51 -0500

    bump terraform version (#1416)

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -6,7 +6,7 @@ docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.6} \
   --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
-  --build-arg TERRAFORM_VERSION=${5:-0.11.14} \
+  --build-arg TERRAFORM_VERSION=${5:-0.11.15} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
   .
 
