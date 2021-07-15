54c5f1bd2b87b0655c0f10bcfd8bd67983ac9808
Nathan Dister <nathan.dister@adhocteam.us>
2021-07-15 17:41:38 -0400

    Delete instead of commenting out code

--- terraform/modules/elb_http_alarms/variables.tf
+++ terraform/modules/elb_http_alarms/variables.tf
@@ -20,33 +20,3 @@ variable "alarm_elb_no_backend_enable" {}
 variable "alarm_elb_no_backend_eval_periods" {}
 variable "alarm_elb_no_backend_period" {}
 variable "alarm_elb_no_backend_threshold" {}
-
-# variable "alarm_elb_high_latency_enable" {}
-# variable "alarm_elb_high_latency_eval_periods" {}
-# variable "alarm_elb_high_latency_period" {}
-# variable "alarm_elb_high_latency_threshold" {}
-
-# variable "alarm_elb_spillover_count_enable" {}
-# variable "alarm_elb_spillover_count_eval_periods" {}
-# variable "alarm_elb_spillover_count_period" {}
-# variable "alarm_elb_spillover_count_threshold" {}
-
-# variable "alarm_elb_surge_queue_length_enable" {}
-# variable "alarm_elb_surge_queue_length_eval_periods" {}
-# variable "alarm_elb_surge_queue_length_period" {}
-# variable "alarm_elb_surge_queue_length_threshold" {}
-
-# variable "alarm_backend_4xx_enable" {}
-# variable "alarm_backend_4xx_eval_periods" {}
-# variable "alarm_backend_4xx_period" {}
-# variable "alarm_backend_4xx_threshold" {}
-
-# variable "alarm_backend_5xx_enable" {}
-# variable "alarm_backend_5xx_eval_periods" {}
-# variable "alarm_backend_5xx_period" {}
-# variable "alarm_backend_5xx_threshold" {}
-
-# variable "alarm_elb_5xx_enable" {}
-# variable "alarm_elb_5xx_eval_periods" {}
-# variable "alarm_elb_5xx_period" {}
-# variable "alarm_elb_5xx_threshold" {}
