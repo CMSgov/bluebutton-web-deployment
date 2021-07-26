fa5bb49d78216f36734568fbf4ce6e74415cc2c4
Don Seymour <don.seymour@adhocteam.us>
2021-07-26 16:22:58 -0500

    update alarm to average gt or equal to .01 percent rate and rename alarm

--- terraform/shared/ses-alerts/main.tf
+++ terraform/shared/ses-alerts/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ses_alerts" {
-    alarm_name              = "bluebutton-SES-bounce-rate-high"
-    comparison_operator     = "GreaterThanThreshold"
+    alarm_name              = "bb-prod-ses-bounce-rate-high"
+    comparison_operator     = "GreaterThanOrEqualToThreshold"
     evaluation_periods      = "1"
     alarm_actions           = ["arn:aws:sns:us-east-1:501705132200:slack-topic"]
-    alarm_description       = "SES bounce rate exceeds 2 in 1 hour"
+    alarm_description       = "SES bounce rate average is greater than or equal to .01% in a 1 hour time period"
     datapoints_to_alarm     = "1"
-    metric_name             = "Bounce"
+    metric_name             = "Reputation.BounceRateBounce"
     namespace               = "AWS/SES"
     period                  = "3600"
     statistic               = "Average"
-    threshold               = "2"
+    threshold               = ".01"
 }
