86edbeec457bc60f554e3527e71ed045325b30dd
Don Seymour <don.seymour@adhocteam.us>
2021-07-28 16:28:40 -0500

    use aws_caller_identity to obscure account id

--- terraform/shared/ses-alerts/main.tf
+++ terraform/shared/ses-alerts/main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_cloudwatch_metric_alarm" "ses_alerts" {
     alarm_name              = "bb-prod-ses-bounce-rate-high"
     comparison_operator     = "GreaterThanOrEqualToThreshold"
     evaluation_periods      = "1"
-    alarm_actions           = ["arn:aws:sns:us-east-1:501705132200:slack-topic"]
+    alarm_actions           = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:slack-topic"]
     alarm_description       = "SES bounce rate average is greater than or equal to .01% in a 1 hour time period"
     datapoints_to_alarm     = "1"
     metric_name             = "Reputation.BounceRate"
