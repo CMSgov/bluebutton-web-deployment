e1cb9d761bd3cd1169cc036fcb4ae08330511b2c
Nathan Dister <nathan.dister@adhoc.team>
2021-08-04 16:42:53 -0400

    Use python 3.7 build container (#1429)
    Co-authored-by: Nathan Dister <nathan.dister@adhocteam.us>

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DOCKER_TAG=${1:-py36-an27-tf11}
+DOCKER_TAG=${1:-py37-an27-tf11}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg PYTHON_VERSION=${2:-3.6} \
+  --build-arg PYTHON_VERSION=${2:-3.7} \
   --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-0.11.15} \
