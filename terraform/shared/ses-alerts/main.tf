c81dd21135da5ae7207bd8fe04450f9bd0534b43
Don Seymour <don.seymour@adhocteam.us>
2021-08-05 11:25:21 -0500

    BB2-718 Monitoring Rework - Implement CloudWatch warnings (#1426)
    * create alarm in shared dir for SES alerts
    
    * update alarm to average gt or equal to .01 percent rate and rename alarm
    
    * typo for metric name correction
    
    * use aws_caller_identity to obscure account id

---
+++ terraform/shared/ses-alerts/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudwatch_metric_alarm" "ses_alerts" {
+    alarm_name              = "bb-prod-ses-bounce-rate-high"
+    comparison_operator     = "GreaterThanOrEqualToThreshold"
+    evaluation_periods      = "1"
+    alarm_actions           = ["arn:aws:sns:us-east-1:${data.aws_caller_identity.current.account_id}:slack-topic"]
+    alarm_description       = "SES bounce rate average is greater than or equal to .01% in a 1 hour time period"
+    datapoints_to_alarm     = "1"
+    metric_name             = "Reputation.BounceRate"
+    namespace               = "AWS/SES"
+    period                  = "3600"
+    statistic               = "Average"
+    threshold               = ".01"
+}
