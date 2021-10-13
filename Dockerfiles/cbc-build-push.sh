170f3512a24d0d0d8d8a1eddc801b90198719b6e
Don Seymour <don.seymour@adhocteam.us>
2021-10-13 12:02:41 -0500

    update build container with tf12

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-DOCKER_TAG=${1:-py37-an27-tf11}
+DOCKER_TAG=${1:-py37-an27-tf12}
 
 docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.7} \
   --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
-  --build-arg TERRAFORM_VERSION=${5:-0.11.15} \
+  --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
   .
 
