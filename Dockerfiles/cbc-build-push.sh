f0e234b12178082f53e374968ed768ad135b50de
Don Seymour <don.seymour@adhocteam.us>
2022-06-07 16:07:56 -0500

    changing combinations hangs with ans2922

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,11 +3,11 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py3810-an2922-tf12-boto3-botocore-jinja2}
+DOCKER_TAG=${1:-py3810-an27-tf12-boto3-botocore-jinja2}
 
 docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.8.10} \
-  --build-arg ANSIBLE_VERSION=${3:-2.9.22} \
+  --build-arg ANSIBLE_VERSION=${3:-2.7} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
