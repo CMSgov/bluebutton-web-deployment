044521e35290871b96cabc05edc06adbecf4d4fd
Don Seymour <don.seymour@adhocteam.us>
2022-06-08 09:53:24 -0500

    try 37 buster

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,10 +3,10 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py38buster-an27-tf12-boto3-botocore}
+DOCKER_TAG=${1:-py37buster-an27-tf12-boto3-botocore}
 
 docker build --pull --no-cache --file Dockerfile.cbc-build \
-  --build-arg PYTHON_VERSION=${2:-3.8-buster} \
+  --build-arg PYTHON_VERSION=${2:-3.7-buster} \
   --build-arg ANSIBLE_VERSION=${3:-2.7} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
