ffb9d42352a941388cee3377bb06fc7073a3fa4f
Don Seymour <don.seymour@adhocteam.us>
2023-01-20 11:07:33 -0500

    testing sg rules on aws config

--- terraform/test_canary/main.tf
+++ terraform/test_canary/main.tf
@@ -2,43 +2,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "template_file" "user_data" {
-  template = file("${path.module}/templates/user_data.tpl")
+# data "template_file" "user_data" {
+#   template = file("${path.module}/templates/user_data.tpl")
 
-  vars = {
-    env                   = lower(var.env)
-    bucket                = var.app_config_bucket
-    static_content_bucket = var.static_content_bucket
-  }
-}
-
-
-
-resource "aws_instance" "test_canary_app" {
-
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  subnet_id     = var.subnet_id
-  #user_data = "${file("${path.module}/templates/user_data")}"
+#   vars = {
+#     env                   = lower(var.env)
+#     bucket                = var.app_config_bucket
+#     static_content_bucket = var.static_content_bucket
+#   }
+# }
 
 
-  user_data               = data.template_file.user_data.rendered
-  iam_instance_profile    = var.iam_instance_profile
 
-  vpc_security_group_ids  = [
-    var.vpc_sg_id,
-    aws_security_group.allow_ci_ssh.id,
-    #var.vpc_sg_id_ci
-  ]
+# resource "aws_instance" "test_canary_app" {
 
-  associate_public_ip_address = false
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
 
-  tags = {
-    Name = "bb-test-canary"
-  }
-}
+#   subnet_id     = var.subnet_id
+#   #user_data = "${file("${path.module}/templates/user_data")}"
+
+
+#   user_data               = data.template_file.user_data.rendered
+#   iam_instance_profile    = var.iam_instance_profile
+
+#   vpc_security_group_ids  = [
+#     var.vpc_sg_id,
+#     aws_security_group.allow_ci_ssh.id,
+#     #var.vpc_sg_id_ci
+#   ]
+
+#   associate_public_ip_address = false
+
+#   tags = {
+#     Name = "bb-test-canary"
+#   }
+# }
 
 # create the CI security group for canary deploy.
 # Once canary instance is manually/automatically 
@@ -64,6 +64,6 @@ resource "aws_security_group" "allow_ci_ssh" {
 
 #Assign Private IP to Output variable
 
-  output "private_ip" {
-    value = aws_instance.test_canary_app.private_ip
-  }
+  # output "private_ip" {
+  #   value = aws_instance.test_canary_app.private_ip
+  # }
