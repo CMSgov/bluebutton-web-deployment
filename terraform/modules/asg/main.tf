876961da0f286d2b991522d425951842af0e069c
Don Seymour <don.seymour@adhocteam.us>
2023-04-18 09:20:31 -0500

    bump up capacity timeout for asg

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -87,7 +87,7 @@ resource "aws_autoscaling_group" "main" {
   min_elb_capacity          = var.asg_min
   health_check_grace_period = 400
   health_check_type         = "ELB"
-  wait_for_capacity_timeout = "20m"
+  wait_for_capacity_timeout = "30m"
   vpc_zone_identifier       = data.aws_subnet_ids.app.ids
   launch_configuration      = aws_launch_configuration.app.name
   load_balancers            = var.elb_names
