d3a59add71a949695adf6ba976012ac84f579b36
Don Seymour <don.seymour@adhocteam.us>
2023-05-10 15:05:35 -0400

    convert cwl subscrip filter to workspace

---
+++ terraform/workspaces/cwl_subscrip_filter/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Copy/paste & customize variables to add new subscription filters for BFD insights
+#
+# NOTE: this subscription filter module is setup for the BFD account, if BFD is not
+# the desired account, we should add some logic here for the account number
+
+module "cwl_subscrip_filter" {
+  source          = "../../modules/cwl_subscrip_filter"
+  filter_name     = "${terraform.workspace}-perf-mon"
+  bfd_acct        = var.bfd_acct
+  log_group_name  = "/bb/${terraform.workspace}/app/perf_mon.log"
+}
