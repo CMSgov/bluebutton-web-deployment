c862a98733c012d6fc8250a2a7e0edd13122c384
Jim Fuqian <fuqian_80231@yahoo.com>
2023-06-09 14:37:41 -0700

    [BB2-2336] BB2 Upgrade Python to 3.8 LTS: Change Platinum Image Build Script to pick Python 3.8 Docker (#1501)
    * build iamge with python3.8
    
    * try fix python version in comman.yml
    
    * fix p37 stuff.
    
    * fix runtime error ansible playbook.
    
    * fix loop users.
    
    * fix pipeline agent kubernates default container.
    
    * point AMI container to python38 build.
    
    * add tracing for trouble shoot
    
    * trouble shoot python install on the container.
    
    * debugging trace..
    
    * debugging trace..
    
    * print out python3 version...
    
    * print out python3 version...
    
    * print out python3 version...
    
    * ls all pythons under usr bin ...
    
    * set defaults for playbook build APP AMI.
    
    * yum install python38 and python38-devel
    
    * install extras python3.8, clean up experimental default repo values.
    
    * fix extras install
    
    * fix extras install
    
    * trouble shooting bb2 install.
    
    * probe python and pip on target machine.
    
    * fix unsatisfied dependencies.
    
    * continue fixing install deps
    
    * add probe extras topics
    
    * display std err lins...
    
    * debug...
    
    * try to install p38 devel.
    
    * debug...
    
    * debug...
    
    * debug...
    
    * debug...
    
    * clean up.
    
    * revert to root registry name
    
    * install python38 and revert ssh users from master
    
    * revert the ssh users temporarily
    
    * checkout master branch ssh ansible taksk
    
    * use pip38 consistently for installing requirements
    
    * workaround ssh
    
    * revert install reqs command
    
    * and back again
    
    * revert ssh loop again prep for merge
    
    ---------
    
    Co-authored-by: JAMES FUQIAN <jfuqian@ellumen.com>
    Co-authored-by: Don Seymour <don.seymour@adhocteam.us>

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,10 +3,10 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py37-an27-tf12-boto3-botocore}
+DOCKER_TAG=${1:-py38-an27-tf12-boto3-botocore}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg PYTHON_VERSION=${2:-3.7} \
+  --build-arg PYTHON_VERSION=${2:-3.8} \
   --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
