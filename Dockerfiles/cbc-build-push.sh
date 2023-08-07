0def3a11f40c3f1197da32a77e2f0c82de139925
Don Seymour <don.seymour@adhocteam.us>
2023-08-07 12:20:37 -0400

    Revert "debug"
    This reverts commit 0f8cfd18bd4764bcf8e7a7719510728e4710587b.

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,11 +3,11 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py38-ans29-tf12-boto3-botocore}
+DOCKER_TAG=${1:-py38-an27-tf12-boto3-botocore}
 
 docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.8} \
-  --build-arg ANSIBLE_VERSION=${3:-2.9} \
+  --build-arg ANSIBLE_VERSION=${3:-2.7.18} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
