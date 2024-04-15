f095c2b5cfff21f543633d373ee988e94642fe1a
Noor <noorulla@gmail.com>
2024-04-15 10:54:21 -0500

    Revert "Revert "IAM policy Sync with Terraform""
    This reverts commit f137f4eca0b7a74f25e42e47ada9e04f18c25d2a.

--- terraform/modules/iam_param_store/main.tf
+++ terraform/modules/iam_param_store/main.tf
@@ -13,20 +13,26 @@ resource "aws_iam_policy" "app_secrets_mgr" {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Sid": "VisualEditor0",
             "Effect": "Allow",
             "Action": [
-                "secretsmanager:ListSecrets",
+                "secretsmanager:GetSecretValue",
                 "secretsmanager:DescribeSecret",
-                "secretsmanager:GetSecretValue"
+                "secretsmanager:ListSecretVersionIds"
             ],
-            "Resource": [
-                "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:/bb2/${var.env}/app/*"
-            ]
-        }
+            "Resource": "arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:/bb2/${var.env}/app/*"
+        },
+       {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "secretsmanager:ListSecrets",
+            "Resource": "*"
+       }
     ]
 }
 EOF
 }
+
 
 resource "aws_iam_role_policy_attachment" "app_param_store" {
   role       = data.aws_iam_role.app.id
