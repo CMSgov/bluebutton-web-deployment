0a5c36c2c004466352c57a53af018b3493afbcec
noor1027 <noorulla@gmail.com>
2024-05-08 11:15:14 -0500

    Bb2 2777 terraform upgrade deployment t (#1534)
    * Update backend.tf
    
    * Upgrade TF to 1.8.2
    
    * Update Jenkinsfile.cbc-deploy-app-ami
    
    * Update Jenkinsfile.cbc-deploy-app-ami
    
    * Update cbc-build-push.sh
    
    * Update cbc-build.yaml
    
    * updating Docker tags
    
    * Removed backup TF in S3 bucket
    
    * Delete tfplan.txt

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,13 +3,13 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py38-ans29-awscol620-tf12-tgrunt-boto3-botocore}
+DOCKER_TAG=${1:-py38-ans29-awscol620-tf18-tgrunt-boto3-botocore}
 
 docker build --file Dockerfile.cbc-build \
   --build-arg PYTHON_VERSION=${2:-3.8} \
   --build-arg ANSIBLE_VERSION=${3:-2.9} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
-  --build-arg TERRAFORM_VERSION=${5:-0.12.31} \
+  --build-arg TERRAFORM_VERSION=${5:-1.8.2} \
   --build-arg AWS_COLLECTION_VERSION=${6:-6.2.0} \
   --build-arg TERRAGRUNT_VERSION=${7:-0.51.9} \
   --tag public.ecr.aws/f5g8o1y9/bb2-cbc-build:${DOCKER_TAG} \
