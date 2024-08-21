45c1919ee998ac1da13738573c8f140aa85cc298
Noor <noorulla@gmail.com>
2024-08-21 11:58:51 -0500

    Python upgrade for Docker build

--- Dockerfiles/cbc-build-push.sh
+++ Dockerfiles/cbc-build-push.sh
@@ -3,10 +3,10 @@
 # TO-DO: pin versions on boto3 and botocore if requested.
 # Versions at time of BB2-1124 dev: boto3==1.21.28 botocore==1.24.28
 
-DOCKER_TAG=${1:-py38-ans29-awscol620-tf18-tgrunt-boto3-botocore}
+DOCKER_TAG=${1:-py311-ans29-awscol620-tf18-tgrunt-boto3-botocore}
 
 docker build --file Dockerfile.cbc-build \
-  --build-arg PYTHON_VERSION=${2:-3.8} \
+  --build-arg PYTHON_VERSION=${2:-3.11.6} \
   --build-arg ANSIBLE_VERSION=${3:-2.9} \
   --build-arg PACKER_VERSION=${4:-1.6.5} \
   --build-arg TERRAFORM_VERSION=${5:-1.8.2} \
